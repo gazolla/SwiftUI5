9e35a92a819d09c72293e887bb10f80207f3b8b0
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2023-06-08 21:34:31 -0300

    add new files

---
+++ SwiftUI5/STransitionView.swift
@@ -0,0 +1,18 @@
+//
+//  STransitionView.swift
+//  SwiftUI5
+//
+//  Created by sebastiao Gazolla Costa Junior on 08/06/23.
+//
+
+import SwiftUI
+
+struct STransitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    STransitionView()
+}
