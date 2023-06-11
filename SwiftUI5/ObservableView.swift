04096c3fb9848b2f6ac7cb54acc159e819524780
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2023-06-11 11:28:15 -0300

    add observable

---
+++ SwiftUI5/ObservableView.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableView.swift
+//  SwiftUI5
+//
+//  Created by sebastiao Gazolla Costa Junior on 09/06/23.
+//
+
+import SwiftUI
+
+struct ObservableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableView()
+}
