a81b1deec5976ed437b407276537de952fda9f4a
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2023-06-17 13:59:37 -0300

    add new files

---
+++ SwiftUI5/AnimateSymbolsView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimateSymbolsView.swift
+//  SwiftUI5
+//
+//  Created by sebastiao Gazolla Costa Junior on 16/06/23.
+//
+
+import SwiftUI
+
+struct AnimateSymbolsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimateSymbolsView()
+}
